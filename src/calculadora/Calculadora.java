f6998a0ef311e68f5c0018066da5d42870e04aad
X541U <X541U@LAPTOP-8M3QRRGJ>
2018-09-26 16:36:33 +0200

    ara suma i resta

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -8,13 +8,24 @@ public class Calculadora {
 
 
     public static void main(String[] args) {
+        int variableInutil1, variableInutil2;
         Scanner in = new Scanner(System.in);
         System.out.print("i = ");
         int i= in.nextInt();
-        System.out.print("\nu = ");
+        System.out.print("u = ");
         int u = in.nextInt();
-        System.out.println("\n"+i + " & " + u);
+        System.out.println(""+i + " + " + u + " = " + suma(i,u));
+        System.out.println(""+i + " - " + u + " = " + resta(i,u));
+
                 
+    }
+    private static int suma(int i, int u)
+    {
+        return i+u;
+    }
+    private static int resta(int i, int u)
+    {
+        return i-u;
     }
     
 }
