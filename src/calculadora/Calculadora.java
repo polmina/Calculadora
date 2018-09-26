c05eca91025ea30104ad8e65c2bbc422062a2d23
X541U <X541U@LAPTOP-8M3QRRGJ>
2018-09-26 16:19:57 +0200

    ara demana numerets

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,11 +1,20 @@
 
 package calculadora;
 
+import java.util.Scanner;
+
 
 public class Calculadora {
 
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("i = ");
+        int i= in.nextInt();
+        System.out.print("\nu = ");
+        int u = in.nextInt();
+        System.out.println("\n"+i + " & " + u);
+                
     }
     
 }
